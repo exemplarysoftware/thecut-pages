36526da3ddce0e59099f828a2012ea6309df9395
Matt Austin <matt@busara.com.au>
2010-08-10 16:50:30 +0800

    Added sitemaps file.

---
+++ sitemaps.py
@@ -0,0 +1,14 @@
+from django.contrib.sitemaps import Sitemap
+from pages.models import Page
+
+
+class PageSitemap(Sitemap):
+    """Sitemaps.org XML sitemap."""
+    def items(self):
+        return Page.objects.active()
+    
+    def lastmod(self, obj):
+        return obj.updated_at
+
+sitemaps = {'pages': PageSitemap}
+
