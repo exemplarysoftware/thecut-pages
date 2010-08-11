edb4cc6d37c882db2faaf4d38afeaae298fa10a0
Matt Austin <matt@busara.com.au>
2010-08-11 08:15:36 +0800

    Sitemap is now generated by objects filtered by site.

--- sitemaps.py
+++ sitemaps.py
@@ -1,11 +1,13 @@
 from django.contrib.sitemaps import Sitemap
+from django.contrib.sites.models import Site
 from pages.models import Page
 
 
 class PageSitemap(Sitemap):
     """Sitemaps.org XML sitemap."""
     def items(self):
-        return Page.objects.active()
+        site = Site.objects.get_current()
+        return Page.objects.active().filter(site=site)
     
     def lastmod(self, obj):
         return obj.updated_at
