1f93b08af9d0d7450f78e9643fbcdd0e565de709
Matt Austin <matt@busara.com.au>
2010-09-23 12:16:55 +0800

    Switched from adding filter methods to a manager, to adding them to a QuerySet class defined on the AbstractPage model itself. Any models which inherit from AbstractPage will need to import QuerySetManager, and define 'objects = QuerySetManager()' in order for them to continue functioning as expected.

--- utils.py
+++ utils.py
@@ -1,17 +1,20 @@
 from django.template.defaultfilters import slugify
 
 
-def generate_unique_url(text, model, iteration=0):
-    """Generate a unique url for a model from the provided text."""
-    url = slugify(text)
+def generate_unique_slug(text, model, queryset=None, iteration=0):
+    """Generate a unique slug for a model from the provided text."""
+    if queryset is None:
+        queryset = model.objects
+    slug = slugify(text)
     if iteration > 0:
-        url = '%s-%s' %(iteration, url)
-    url = '/%s' %(url[:49])
+        slug = '%s-%s' %(iteration, slug)
+    slug = slug[:50]
     try:
-        print model.objects.get(url=url)
+        print queryset.get(slug=slug)
     except model.DoesNotExist:
-        return url
+        return slug
     else:
         iteration += 1
-        return generate_unique_url(text, model, iteration=iteration)
+        return generate_unique_slug(text, model, queryset=queryset,
+            iteration=iteration)
 
