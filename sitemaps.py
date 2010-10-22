6d50c893ad8c0cc16e5647621fb3184f12e9b49c
Matt Austin <matt@busara.com.au>
2010-10-22 13:11:02 +0800

    Added sitemap for SitesPage model.

--- sitemaps.py
+++ sitemaps.py
@@ -1,5 +1,5 @@
 from django.contrib.sitemaps import Sitemap
-from pages.models import Page
+from pages.models import Page, SitesPage
 
 
 class PageSitemap(Sitemap):
@@ -10,5 +10,14 @@ class PageSitemap(Sitemap):
     def lastmod(self, obj):
         return obj.updated_at
 
-sitemaps = {'pages': PageSitemap}
+
+class SitesPageSitemap(Sitemap):
+    """Sitemaps.org XML sitemap."""
+    def items(self):
+        return SitesPage.objects.current_site().indexable()
+    
+    def lastmod(self, obj):
+        return obj.updated_at
+
+sitemaps = {'pages': PageSitemap, 'sitespages': SitesPageSitemap}
 
