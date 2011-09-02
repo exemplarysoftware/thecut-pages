f1e0abdfed5f1002657c79add58e2e6b2fb341e8
Matt Austin <matt.austin@thecut.net.au>
2011-09-02 10:00:37 +0800

    Class-based views. Namespaced urls moved in to urls.py.

--- thecut/pages/middleware.py
+++ thecut/pages/middleware.py
@@ -10,7 +10,7 @@ class PageMiddleware(object):
         if response.status_code != 404:
             return response # No need to check for a page on non-404 responses.
         try:
-            return page(request, request.path_info)
+            return page(request, slug=request.path_info)
         # Return the original response if any errors happened. Because this
         # is a middleware, we can't assume the errors will be caught elsewhere.
         except Http404:
