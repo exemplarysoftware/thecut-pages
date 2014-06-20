5be5809208d76f006baec03e5733d0a1a94487e2
Matt Austin <matt.austin@thecut.net.au>
2014-06-20 11:00:47 +0800

    Added PageFactory and PageFakerFactory, with 'pages create' management command.

---
+++ thecut/pages/management/commands/pages.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from django.core.management.base import BaseCommand, CommandError
+from optparse import make_option
+
+
+class Command(BaseCommand):
+
+    args = '[create]'
+
+    option_list = BaseCommand.option_list + (
+        make_option('--quantity',
+            action='store',
+            type='int',
+            dest='quantity',
+            default=1,
+            help='Number of instances to create'),
+        )
+
+    help = 'create: Create page(s) with fake data'
+
+    def handle(self, command, *args, **options):
+
+        if command not in ['create']:
+            raise CommandError('"{0}" is not a valid argument'.format(command))
+
+        if command == 'create':
+            from ...factories import PageFakerFactory
+            quantity = options['quantity']
+            PageFakerFactory.create_batch(quantity)
+
+            output = 'Created {0} page(s) with fake data'.format(quantity)
+            try:
+                self.stdout.write(output)
+            except AttributeError:
+                print output
