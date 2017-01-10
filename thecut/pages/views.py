a7bb4740d8ca6b462b515cc28a6ad4f6b072e063
Mark Lockett <mark.lockett@thecut.net.au>
2017-01-10 08:39:17 +0800

    Comment out debugging print statement Refs #9326

--- thecut/pages/views.py
+++ thecut/pages/views.py
@@ -16,7 +16,7 @@ class DetailView(DetailView):
 
     def get_queryset(self):
         url = self.kwargs.get('slug', None)
-        print("DetailView get_queryset url=", url)
+        #print("DetailView get_queryset url=", url)
         return Page.objects.current_site().active().filter(Q(url=url)|
                                                            Q(url='/' + url))
 
