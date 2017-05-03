a8b5e72a0ca7f81f609b2efbcf7117c3f9cd27f9
Mark Lockett <mark.lockett@thecut.net.au>
2017-05-03 11:06:51 +0800

    Upgrade to Django 1.11 Refs #10603

--- thecut/pages/urls.py
+++ thecut/pages/urls.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
-from django.conf.urls import include, url
+from django.conf.urls import url
 from thecut.pages import views
 
 
@@ -11,5 +11,3 @@ urlpatterns = [
     url(r'^(?P<slug>[\w/-]+)$', views.DetailView.as_view(),
         name='page_detail'),
 ]
-
-
